a896194731fb64a7cb964383d7603c2502e7d9f3
camilybqueiroz <rm94856@estudante.fieb.edu.br>
2025-04-03 08:25:41 -0300

    envio de parâmetros

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ function App(){
 
   const [nome, setNome] = useState('Camily')
 
-    const Formulario = () => {
+    const Formulario = (props) => {
         return (
         <div>
-          <div>
+          
         <h3>Pizzaria 2C</h3>
         <input
+        placeholder={props.sombra}
         className="nome"
         onChange={(e)=>{setNome(e.target.value)}}
         nome="nome"
         type="text" />
         <button
-        onClick={()=>{alert(nome);}}
+        onClick={()=>{(props.mensagem != null) alert(props.mensagem)
+                                    : alert("TEXTO PADRÃO")}}
         classname="botao">
-          CLIQUE AQUI
+         {(props.textoBotao != null) ? props.textoBotao : "CLIQUE AQUI"}
         </button>
         </div>
   );
@@ -28,7 +30,12 @@ function App(){
     return (
       <div>
         <h3>Pizzaria 2C</h3>
+        <Formulario sombra="Digite seu nome..." textoBotao="ENVIAR NOME"/>
+        <Formulario sombra="Digite seu e-mail..." textoBotao="E-MAIL"/>
+        <Formulario sombra="(11) 90000-0000" textoBotao="CELULAR"/>
         <Formulario />
+        <Formulario />
+
       </div>
     )
 }
